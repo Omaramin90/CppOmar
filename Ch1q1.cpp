453158a78c7163042974cd7f919d530083f900d1
Omar Amin <oamincc@gmail.com>
2020-09-11 17:36:19 -0400

    first commit

---
+++ Ch1q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+    cout << "Enter Price of Unit -";
+    cin >> a;
+
+    cout << "Enter Number of units -";
+    cin >> b;
+
+    cout << "Total is -" << a*b;
+    
+
+return 0;
+}
